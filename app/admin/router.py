95c4715198aa197a896f3a0ef865aaf2acb80249
xy3 <120182408@qq.com>
2024-11-11 15:50:37 +0800

    商品分类

--- app/admin/router.py
+++ app/admin/router.py
@@ -8,5 +8,9 @@ admin_router.include_router(index_router)
 from admin.controller.user import router as user_router
 admin_router.include_router(user_router)
 
+from admin.controller.category import router as category_router
+admin_router.include_router(category_router)
+
+
 from admin.controller.login import router as login_router
 admin_router.include_router(login_router)
