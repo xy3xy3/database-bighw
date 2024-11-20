24bc3fc56eebab0668940f427101834fb435b6f2
xy3 <120182408@qq.com>
2024-11-21 00:07:43 +0800

    初始化新版本说明

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # 数据库配置
+    # 主数据库配置
     db_host: str = "172.18.198.221"
     db_port: str = "5432"
     db_user: str = "superuser"
@@ -10,8 +10,13 @@ class Settings(BaseSettings):
     db_name: str = "postgres"
     db_schema: str = "hw"
 
-    # 其他配置（如果有）
-    # ...
+    # dataVec数据库配置
+    vec_db_host = "172.18.35.21"
+    vec_db_port = "5433"
+    vec_db_user = "gaussdb"
+    vec_db_password = "OGSql@123"
+    vec_db_name = "postgres"
+    vec_db_schema = "postgres"
 
     class Config:
         env_file = ".env"  # 如果需要从环境变量文件中读取配置
