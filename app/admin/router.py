a63e1ea459036da5a587790100ee89af213856d8
Harvey Mo <mozh6@mail2.sysu.edu.cn>
2024-11-22 21:09:00 +0800

    fix router

--- app/admin/router.py
+++ app/admin/router.py
@@ -5,13 +5,6 @@ admin_router = APIRouter()
 from admin.controller.index import router as index_router
 admin_router.include_router(index_router)
 
-from admin.controller.user import router as user_router
-admin_router.include_router(user_router)
-
-from admin.controller.category import router as category_router
-admin_router.include_router(category_router)
-
-
 from admin.controller.login import router as login_router
 admin_router.include_router(login_router)
 
