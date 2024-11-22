50ede5b044e061d72e3e698f15e2f786ddaf68f3
xy3 <120182408@qq.com>
2024-11-22 20:38:57 +0800

    fixrouter

--- app/admin/router.py
+++ app/admin/router.py
@@ -5,19 +5,12 @@ admin_router = APIRouter()
 from admin.controller.index import router as index_router
 admin_router.include_router(index_router)
 
-from admin.controller.user import router as user_router
-admin_router.include_router(user_router)
-
-from admin.controller.category import router as category_router
-admin_router.include_router(category_router)
 
 
 from admin.controller.login import router as login_router
 admin_router.include_router(login_router)
 
 
-from admin.controller.order import router as order_router
-admin_router.include_router(order_router)
 
 from admin.controller.model import router as model_router
 admin_router.include_router(model_router)
