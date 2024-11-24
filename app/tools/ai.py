cb8ba174bf33fa7d9a1e411af424ca48a3647c08
xy3 <120182408@qq.com>
2024-11-24 15:54:13 +0800

    对话辅助函数

--- app/tools/ai.py
+++ app/tools/ai.py
@@ -4,13 +4,13 @@ from openai import AsyncOpenAI
 
 class ai:
     def __init__(self,api_key:str,base_url:str):
-        self.ai = AsyncOpenAI(
+        self.client = AsyncOpenAI(
             api_key=api_key,
             base_url=base_url,
         )
     async def embedding(self,model:str,text:str)->list:
         try:
-            embedding = await self.ai.embeddings.create(
+            embedding = await self.client.embeddings.create(
                 model=model,
                 input=text,
             )
@@ -18,17 +18,43 @@ class ai:
         except Exception as e:
             print(e)
             return None
+    async def chat(self,model:str,messages:str,stream:bool=False)->list:
+        try:
+            res = await self.client.chat.completions.create(
+                messages=messages,
+                model=model,
+                stream = stream,
+            )
+            if stream:
+                async for chunk in res:
+                    '''
+ChatCompletionChunk(id='20241124155243734b4ca3e3af48ae', choices=[Choice(delta=ChoiceDelta(content='', function_call=None, refusal=None, role='assistant', tool_calls=None), finish_reason='stop', index=0, logprobs=None)], created=1732434763, model='glm-4-flash', object=None, service_tier=None, system_fingerprint=None, usage=CompletionUsage(completion_tokens=50, prompt_tokens=9, total_tokens=59, completion_tokens_details=None, prompt_tokens_details=None))
+                    '''
+                    # print(chunk.choices[0].delta.content or "", end="")
+                    print(chunk)
+            else:
+                return res.choices[0].message.content
+        except Exception as e:
+            print(e)
+            return None
 
 async def test():
     key = "7305f8f725fd64362176a8cc68f1d909.fHTbqG2ArlpGP901"
     url = "https://open.bigmodel.cn/api/paas/v4"
-    model = "embedding-3"
-    text = "你好"
     aiApi = ai(key,url)
-    res = await aiApi.embedding(model,text)
-    # 获取对象类型
-    print(type(res))
-    print(res)
+    #测试embedding
+    # model = "embedding-3"
+    # text = "你好"
+    # res = await aiApi.embedding(model,text)
+    # print(type(res))
+    # print(res)
+    # 测试chat
+    model = "glm-4-flash"
+    msg = [
+        {"role": "user", "content": "你好,中山大学在哪里"},
+    ]
+    res = await aiApi.chat(model,msg,True)
+
 if __name__ == '__main__':
     import asyncio
     asyncio.run(test())
